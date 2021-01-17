61bc7ad3de3c1615c7d63b35a2c29ea9c3944d1c
multibankfxFatihPolatli <fatih.polatli@multibankfx.com>
2021-01-17 15:41:06 +0300

    swagger config fix for api path

--- src/main/java/com/frankshop/api/config/SwaggerConfig.java
+++ src/main/java/com/frankshop/api/config/SwaggerConfig.java
@@ -14,6 +14,6 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any()).build();
+                .paths(PathSelectors.ant("/api/**")).build();
     }
 }
